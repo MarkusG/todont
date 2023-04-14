c832ae5e3393c7039645abc44351c1b5e0e23281
Mark Gross <mark.gross2001@gmail.com>
2023-04-13 23:00:08 -0700

    Add basic UI

--- todont-client/src/App.js
+++ todont-client/src/App.js
@@ -1,6 +1,52 @@
+import './App.css';
+
 function App() {
   return (
-      <h1>Hello world!</h1>
+      <div className="container">
+        <h1>Todos</h1>
+        <div className="todos">
+          <div className="todo">
+            <div>
+              <h2>Todo title 1</h2>
+              <p>This is a short description of the todo</p>
+            </div>
+            <div className="todo-buttons">
+              <span className="check"></span>
+              <span className="x"></span>
+            </div>
+          </div>
+          <div className="todo">
+            <div>
+              <h2>Todo title 1</h2>
+              <p>This is a short description of the todo</p>
+            </div>
+            <div className="todo-buttons">
+              <span className="check"></span>
+              <span className="x"></span>
+            </div>
+          </div>
+          <div className="todo">
+            <div>
+              <h2>Todo title 1</h2>
+              <p>This is a short description of the todo</p>
+            </div>
+            <div className="todo-buttons">
+              <span className="check"></span>
+              <span className="x"></span>
+            </div>
+          </div>
+          <div className="todo">
+            <div>
+              <h2>Todo title 1</h2>
+              <p>This is a short description of the todo</p>
+            </div>
+            <div className="todo-buttons">
+              <span className="check"></span>
+              <span className="x"></span>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
