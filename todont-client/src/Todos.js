6586caef709666de89947293459f75fec4c2394a
Mark Gross <mark.gross2001@gmail.com>
2023-05-21 19:25:00 -0700

    Add error handling

--- todont-client/src/Todos.js
+++ todont-client/src/Todos.js
@@ -4,12 +4,26 @@ import Todo from './Todo.js';
 
 export default function Todos() {
     const [todos, setTodos] = useState([]);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
       fetch('http://localhost:3001/todos')
-        .then((response) => response.json())
+        .then((response) => {
+            if (response.ok)
+                return response.json();
+            setError(`An error occured when fetching todos
+                (${response.statusText})`);
+        })
         .then((data) => { setTodos(data); })
-        .catch((e) => { console.log(e.message); });
+        .catch((e) => {
+            console.log(e.message);
+            setError(`An error occurred when fetching todos (${e.message})`);
+        });
     }, []);
+
+    if (error != null)
+        return (<p>{error}</p>);
+
     return (
         <div className="flex flex-col gap-4">
           {todos.map((t) => (
