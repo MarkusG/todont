f4f523f50cd5d91effd7210d30d116fbd236c921
Mark Gross <mark.gross2001@gmail.com>
2023-06-22 19:23:40 -0700

    Navigate to /todos from root

--- todont-client/src/Root.js
+++ todont-client/src/Root.js
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import './Styles.css';
 import Sidebar from './Sidebar.js';
 
 function App() {
+    const navigate = useNavigate();
     useEffect(() => {
         document.title = 'Todont';
-    }, []);
+        navigate('/todos');
+    }, [navigate]);
 
     return (
         <div className="flex flex-col md:flex-row h-screen w-screen bg-gray-100">
