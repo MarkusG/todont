c13dc337bb61917867c56383ff94464cf5efb904
Mark Gross <mark.gross2001@gmail.com>
2023-06-22 17:11:59 -0700

    Implement todo creation

--- todont-client/src/CreateTodo.js
+++ todont-client/src/CreateTodo.js
@@ -1,17 +1,63 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function CreateTodo() {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const navigate = useNavigate();
+
+    function submit() {
+        // TODO validation
+        fetch(`http://localhost:3001/todos`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ title, content })
+        }).then((response) => {
+            if (response.ok)
+                navigate('/todos');
+        }).catch((e) => {
+            console.log(e.message);
+        });
+    }
+
     return (
         <div className="w-full h-full p-4 bg-gray-50 border shadow-md">
-            <Link to={'/todos'}>
-                <button className="p-2 text-gray-500">
-                    <FontAwesomeIcon icon={solid('arrow-left')} size="md" className="mr-2"/>
-                    Back
+            <div className="flex justify-between mb-2">
+                <Link to={'/todos'} draggable="false">
+                    <button className="block p-2 text-gray-500">
+                        <FontAwesomeIcon icon={solid('arrow-left')} size="md" className="mr-2"/>
+                        Back
+                    </button>
+                </Link>
+                <button className="block p-2 bg-primary-500 text-white rounded" onClick={submit}>
+                    Create
                 </button>
-            </Link>
+            </div>
+            <div className="m-auto min-w-fit max-w-[500px]">
+                <div className="mb-2">
+                    <label for="title" className="text-lg">Title:</label>
+                    <input
+                        id="title"
+                        name="title"
+                        className="block w-full p-1 bg-white border rounded"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}/>
+                </div>
+                <div className="mb-2">
+                    <label for="content" className="text-lg">Content:</label>
+                    <textarea
+                        id="content"
+                        name="content"
+                        className="block w-full h-[8em] p-1 bg-white border rounded"
+                        value={content}
+                        onChange={e => setContent(e.target.value)}/>
+                </div>
+            </div>
         </div>
     )
 }
--- todont-client/src/CreateTodoButton.js
+++ todont-client/src/CreateTodoButton.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function CreateTodoButton() {
     return (
-        <Link to={'/todos/create'}>
+        <Link to={'/todos/create'} draggable="false">
             <button className="text-xl p-2 rounded bg-primary-500 text-white">
                 <span className="mr-2 plus"></span>
                 Create Todo
