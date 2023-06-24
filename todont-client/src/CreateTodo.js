5a35867f47abd956bcd175ed965802e12479a080
Mark Gross <mark.gross2001@gmail.com>
2023-06-24 15:28:04 -0700

    Add validation to creation page

--- todont-client/src/CreateTodo.js
+++ todont-client/src/CreateTodo.js
@@ -4,19 +4,21 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
+import InputText from './Forms/InputText.js';
+import InputTextArea from './Forms/InputTextArea.js';
+
 export default function CreateTodo() {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+    const [title, setTitle] = useState({ value: "", errors: [] });
+    const [content, setContent] = useState({ value: "", errors: [] });
     const navigate = useNavigate();
 
     function submit() {
-        // TODO validation
         fetch(`http://localhost:3001/todos`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ title, content })
+            body: JSON.stringify({ title: title.value, content: content.value })
         }).then((response) => {
             if (response.ok)
                 navigate('/todos');
@@ -25,37 +27,55 @@ export default function CreateTodo() {
         });
     }
 
+    function updateTitle(title) {
+        let newTitle = { value: title, errors: [] };
+        if (title.length > 50)
+            newTitle.errors.push('Title must be 50 characters or less');
+        setTitle(newTitle)
+    }
+
+    function updateContent(content) {
+        let newContent = { value: content, errors: [] };
+        if (content.length > 1000)
+            newContent.errors.push('Content must be 1000 characters or less');
+        setContent(newContent)
+    }
+
     return (
         <div className="w-full h-full p-4 bg-gray-50 border shadow-md">
             <div className="flex justify-between mb-2">
                 <Link to={'/todos'} draggable="false">
                     <button className="block p-2 text-gray-500">
-                        <FontAwesomeIcon icon={solid('arrow-left')} size="md" className="mr-2"/>
+                        <FontAwesomeIcon icon={solid('arrow-left')} className="mr-2"/>
                         Back
                     </button>
                 </Link>
-                <button className="block p-2 bg-primary-500 text-white rounded" onClick={submit}>
+                <button className="block p-2 bg-primary-500 text-white rounded"
+                    disabled={title.errors.length !== 0 || content.errors.length !== 0}
+                    onClick={submit}>
                     Create
                 </button>
             </div>
             <div className="m-auto min-w-fit max-w-[500px]">
                 <div className="mb-2">
-                    <label for="title" className="text-lg">Title:</label>
-                    <input
+                    <label htmlFor="title" className="text-lg">Title:</label>
+                    <InputText
                         id="title"
                         name="title"
                         className="block w-full p-1 bg-white border rounded"
-                        value={title}
-                        onChange={e => setTitle(e.target.value)}/>
+                        value={title.value}
+                        errors={title.errors}
+                        onChange={e => updateTitle(e)}/>
                 </div>
                 <div className="mb-2">
-                    <label for="content" className="text-lg">Content:</label>
-                    <textarea
+                    <label htmlFor="content" className="text-lg">Content:</label>
+                    <InputTextArea
                         id="content"
                         name="content"
                         className="block w-full h-[8em] p-1 bg-white border rounded"
-                        value={content}
-                        onChange={e => setContent(e.target.value)}/>
+                        value={content.value}
+                        errors={content.errors}
+                        onChange={e => updateContent(e)}/>
                 </div>
             </div>
         </div>
---
+++ todont-client/src/Forms/InputText.js
@@ -0,0 +1,32 @@
+export default function InputText(props) {
+    function borderClass() {
+        if (props.value)
+            return props.errors.length === 0 ? 'border-success-500' : 'border-danger-500';
+        return '';
+    }
+
+    function errorList() {
+        if (props.errors.length === 0)
+            return;
+
+        return (
+            <ul className="mt-1 text-danger-500 list-disc list-inside">
+                {props.errors.map((e) => (
+                    <li>{e}</li>
+                ))}
+            </ul>
+        )
+    }
+
+    return (
+        <>
+            <input
+                id={props.id}
+                name={props.name}
+                className={borderClass() + ' ' + props.className}
+                value={props.value}
+                onChange={e => props.onChange(e.target.value)}/>
+            {errorList()}
+        </>
+    )
+}
